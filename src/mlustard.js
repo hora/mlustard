bd697fc0a5925c003f671b6075fa3efeebf9ebaa
Horatiu Halmaghi <hhalmaghi@protonmail.com>
2021-04-02 18:17:09 -0400

    cleans up some cruft

--- src/mlustard.js
+++ src/mlustard.js
@@ -101,22 +101,16 @@ const analyzeGameEvent = (eventData) => {
 
   let analysis = initAnalysis(eventData);
 
-  steals.check(analysis, eventData);
+  const checkers = [
+    gameStatus,
+    outs,
+    hits,
+    walks,
+    steals,
+  ];
 
-  if(gameStatus.check(analysis, eventData)) {
-    //return analysis;
-  }
-
-  if (outs.check(analysis, eventData)) {
-    //return analysis;
-  }
-
-  if (hits.check(analysis, eventData)) {
-    //return analysis;
-  }
-
-  if (walks.check(analysis, eventData)) {
-    //return analysis;
+  for (const checker of checkers) {
+    checker.check(analysis, eventData);
   }
 
   return analysis;
@@ -126,48 +120,3 @@ module.exports = {
   analyzeGameEvent
 };
 
-    //out: {
-    // true when there is an out on the play
-      //isOut: false,
-      // one of: 'flyout', 'groundOut', 'strikeOut', 'caughtStealing',
-      // 'sacrifice'
-      //kind: null,
-      // true when the out closes out a half inning
-      //isLastOfHalfInning: null,
-      // how many runs were scored on the play
-      //runsScored: 0,
-    //},
-
-    // --- Hits
-    // --------
-    // collects information about the hit on the play
-    //hit: {
-      // true when there is a hit on the play
-      //isHit: false,
-      // one of: 'single', 'double', 'triple', 'homeRun'
-      //kind: null,
-      // the number of runs batted in with this hit
-      //rbi: 0,
-      // whether a Big Bucket was activated by the hit
-      //bigBucket: false,
-    //},
-//
-    // collects information about the steal on the play
-    //steal: {
-      // true when there is a steal on the play
-      //isSteal: false,
-      // true when thief not caught, false otherwise
-      //success: null,
-      // which base was stolen (0-indexed)
-      //baseStolen: null,
-      // how many runs were scored as a result
-      //runsScored: 0,
-    // --- Walks
-    // ---------
-    // collects information about the walk on the play
-    //walk: {
-      // true when there is a walk on the play
-      //isWalk: false,
-      // how many walks were scored as a result
-      //runsScored: 0,
-    //},
